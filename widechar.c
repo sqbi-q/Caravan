481a913fdc30bf5504f2edf9bbc37080ea58c03c
Sebastian <sebasqbi@gmail.com>
2022-07-12 18:13:05 +0200

    Fix pasting; limit characters read by getch()

--- widechar.c
+++ widechar.c
@@ -8,11 +8,25 @@ void getch_wint(int* dest_wint) {
    dest_wint[0] = getch(); /* call blocking getch */ 
    nodelay(stdscr, TRUE); /* now cause non-blocking getch calls */ 
    
+   unsigned ch_len = MAX_WCHAR_SIZE-1;
+   /* https://www.fileformat.info/info/unicode/utf8.htm 
+    * The value of each individual byte indicates its UTF-8 function, as follows:
+    *    00 to 7F hex (0 to 127): first and only byte of a sequence.
+    *    80 to BF hex (128 to 191): continuing byte in a multi-byte sequence.
+    *    C2 to DF hex (194 to 223): first byte of a two-byte sequence.
+    *    E0 to EF hex (224 to 239): first byte of a three-byte sequence.
+    *    F0 to FF hex (240 to 255): first byte of a four-byte sequence. */
+   if (dest_wint[0] <= 0x7F) ch_len = 1; 
+   else if (dest_wint[0] <= 0xBF) ch_len = MAX_WCHAR_SIZE-1;  
+   else if (dest_wint[0] <= 0xDF) ch_len = 2;
+   else if (dest_wint[0] <= 0xEF) ch_len = 3;
+   /* TODO Implement four-byte or/and multi-byte wide character (currently is limited to three bytes) */
+   
    unsigned i=1;
-   dest_wint[i] = getch();
-   while (dest_wint[i] != ERR && i < MAX_WCHAR_SIZE-1) { /* getch until input queue is empty */
-     i++;
+   while (i < ch_len && i < MAX_WCHAR_SIZE-1) { /* getch until input queue is empty */
      dest_wint[i] = getch();
+     if (dest_wint[i] == ERR) break;
+     i++;
    }
    
    dest_wint[i] = '\0';
